6855dc18c445fe138ccebde4dad001779127f6d4
HugoVG <hugovangeijn@gmail.com>
2022-05-31 15:44:50 +0200

    A

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,15 @@ import Button from '@mui/material/Button';
 import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 import zIndex from '@mui/material/styles/zIndex';
 import Typing from './Typing';
-import { FormControlLabel, FormGroup, Paper, Switch } from '@mui/material';
+import { FormControlLabel, FormGroup, Paper, Switch, Typography } from '@mui/material';
 import { animated, SpringValue, useSpring } from 'react-spring';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Label } from '@mui/icons-material';
+import { width } from '@mui/system';
+import BiOsCoop from  './Images/Bi_Os_Coop.png';
+import RDMRaceApp from './Images/Phidippides.png';
+import Makkr from './Images/duck.png';
 
 
 
@@ -25,7 +30,7 @@ function App() {
 	};
 	var from : number = 0;
 	var to : number = 2;
-	const [checked, setChecked] = React.useState(true);
+	const [checked, setChecked] = React.useState(false);
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setChecked(event.target.checked);
 	};
@@ -57,41 +62,63 @@ function App() {
 		}
 	));
 
-	const CBToggle = <FormGroup className='NavItem' style={{ float: "right", color: "#1b76d0" }}>
-		<FormControlLabel control={<Switch checked={checked} defaultChecked onChange={handleChange} />} labelPlacement="start" label="Auto Scroll" />
-	</FormGroup>;
-	const Images = <div style={{ display: "inline" }}>
-		<img height={"200vh"}
-			src="https://github-readme-stats.vercel.app/api/top-langs?username=hugovg&count_private=true&show_icons=true&locale=en&layout=compact&theme=discord_old_blurple"
-			alt="hugovg" />
-		<img height={"200vh"} src="https://github-readme-stats.vercel.app/api?username=hugovg&show_icons=true&locale=en&theme=discord_old_blurple" alt="hugovg" />
+	const CBToggle = 
+		<FormGroup className='NavItem' style={{ float: "right", color: "#1b76d0" }}>
+			<FormControlLabel control={<Switch checked={checked} defaultChecked onChange={handleChange} />} labelPlacement="start" label="Auto Scroll" />
+		</FormGroup>;
+	const GHFrames = <div style={{textAlign: "center", color: "white", textShadow: "black 0.1em 0.1em 0.2em", display: 'inline' }}>
+		<Typography variant="h2">
+			Proficiancy
+		</Typography>
+		<div>
+			<h3 >
+				My Proficancy lies within the .Net ecosystem.<br/>I mostly use C# and have experience with ASP.NET Core,<br/> Entity Framework, WPF, and ASP.NET MVC,<br/> Winforms and ASP.NET Web API.
+			</h3>
+		</div>
+		<div style={{display: "flex", flexDirection: 'row', justifyContent: 'center', flexWrap: "wrap" ,  height: "100%", width: "100%"}}>
+			<div style={{width: "45%",minWidth: '400px', margin: "10px"}}>
+				<Typography variant="h4" height='100px'>
+					Recent most used languages:
+				</Typography>
+				<img width={'100%'}
+					src="https://github-readme-stats.vercel.app/api/top-langs?username=hugovg&count_private=true&show_icons=true&locale=en&layout=compact&theme=discord_old_blurple"
+					alt="hugovg" />
+			</div>
+			<div style={{width: "45%", minWidth: '400px', margin: "10px"}}>
+				<Typography variant="h4"  height='100px'>
+					Github statistics:
+				</Typography>
+				<img width={'100%'} src="https://github-readme-stats.vercel.app/api?username=hugovg&show_icons=true&locale=en&theme=discord_old_blurple" alt="hugovg" />
+			</div>
+		</div>
 	</div>;
 	return (
 		<div className="App">
 			<div className='Background' style={{height: "100vh", position: "absolute"}} />
 			
 			<Parallax ref={parallax} pages={5}>
-				<ParallaxLayer sticky={{ start: from, end: 1.75 }} style={{zIndex:-1}} speed={10}>
+				<ParallaxLayer sticky={{ start: from, end: 2 }} style={{zIndex:-1}} speed={10}>
 					<img className='Birds' src={robin1} width={100} />
 				</ParallaxLayer>
-				<ParallaxLayer  style={{zIndex:-1}} sticky={{ start: 1.75, end: 2.75 }}>
+				<ParallaxLayer  style={{zIndex:-1}} sticky={{ start: 2, end: 4 }}>
 					<img className='Birds' src={robin2} width={100} />
 				</ParallaxLayer>
-				<ParallaxLayer  style={{zIndex:-1}} sticky={{ start: 2.75, end: 4}}>
+				<ParallaxLayer  style={{zIndex:-1}} sticky={{ start: 4, end: 5}}>
 					<img className='Birds' src={robin3} />
 				</ParallaxLayer>
 				<ParallaxLayer sticky={{ start: 0, end: 5 }}
 					style={{ display: 'flex', justifyContent: 'top', alignItems: 'top' }}>
 						<div style={{width: "100%", height: "50px", backgroundColor: "#303030"}}> 
 							<ul className='Navbar' >
-								<li className='NavItem' >
-									<Button className='NavButton' onClick={() => {scroll(0); setChecked(false);}}>Collapse</Button>
-								</li>
+								
 								<li className='NavItem' >
 									<Button className='NavButton' onClick={() => {scroll(0); setChecked(false);}}>About me</Button>
 								</li>
 								<li className='NavItem' >
-									<Button className='NavButton'  onClick={() => {scroll(1); setChecked(false);}}>Languages</Button>
+									<Button className='NavButton'  onClick={() => {scroll(1); setChecked(false);}}>Proficiancy</Button>
+								</li>
+								<li className='NavItem' >
+									<Button className='NavButton' onClick={() => {scroll(2); setChecked(false);}}>Projects</Button>
 								</li>
 								<li className='NavItem' style={{float: "right"}}>
 									{CBToggle}
@@ -99,20 +126,22 @@ function App() {
 							</ul>
 						</div>
 				</ParallaxLayer>
+
+				{/* #region About me */}
 				<ParallaxLayer offset={0} speed={0.1}
 					style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: "black", background: bg }}>
 					<div style={{justifyContent: 'center',textAlign: "center" , alignItems: 'center', color: "white", textShadow: "black 0.1em 0.1em 0.2em" }}  >
-						<h1>HugoVG</h1>
+						<Typography variant="h1">HugoVG</Typography>
 						<div className='Aboutme'>
-							<h2>
-								About Me
-							</h2>
+							<Typography variant="h2">
+								About Me:
+							</Typography>
 							<Typing/> <br/>							 
-							<label style={{justifyContent: 'center', alignItems: 'center', textAlign: "center"}}>
+							<Typography variant='h6' style={{justifyContent: 'center', alignItems: 'center', textAlign: "center"}}>
 								I am a software developer with a passion for creating small and performant applications<br/>
 								I mainly develop in C# and .NET Core, but I am also familiar with JavaScript and TypeScript.<br/>
 								I like Birds and gaming. 
-							</label>
+							</Typography>
 						</div>
 						<animated.div
 							className="Chonk"
@@ -127,7 +156,6 @@ function App() {
 							}}
 						/>
 					</div>
-
 				</ParallaxLayer>
 
 
@@ -136,13 +164,10 @@ function App() {
 						justifyContent: 'center',
 						alignItems: 'center',
 						color: 'white',
-						backgroundColor: '#ff00ff',
+						width: '100vw',						
 					}}>
 					<div>
-						<h1>Proficiancy</h1>
-						<h2>Recent most used languages</h2>
-						{Images}
-
+						{GHFrames}
 					</div>
 				</ParallaxLayer>
 				<ParallaxLayer offset={2} speed={3} style={{
@@ -153,35 +178,60 @@ function App() {
 						justifyContent: 'center',
 						alignItems: 'center'}}>
 						<div style={{width: "25%", height: "100%",display: 'flex',
+							flexDirection: 'column',
 							justifyContent: 'center',
 							alignItems: 'center' }}>
-							<Carousel showArrows={true} autoPlay={true} interval={7000} infiniteLoop={true}>
-								<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center'}}>
-									<img src={robin1} width={100} />
-								</Paper>
-								<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center'}}>	
-									<img src={robin2} width={100} />
-								</Paper>
-								<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center'}}>	
-									<img src={robin2} width={100} />
-								</Paper>
-								<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center'}}>	
-									<img src={robin2} width={100} />
-								</Paper>
-								<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center'}}>	
-									<img src={robin2} width={100} />
-								</Paper>
-							</Carousel>
+								<Typography variant="h2" style={{color: "white", textShadow: "black 0.1em 0.1em 0.2em"}}>
+									Previous Projects
+								</Typography>
+								<div style={{minWidth: "300px", width: "40vw"}}>
+									<Carousel showArrows={false} showThumbs={false} autoPlay={true} interval={4000} infiniteLoop={true}>
+										<Paper elevation={1} style={{ display: 'flex',
+											width: "100%", height: "100%",
+											backgroundColor: '#b6d4f2',
+											color: "white", textShadow: "black 0.1em 0.1em 0.2em",
+											justifyContent: 'center',
+											alignItems: 'center'}}>
+												<div>
+													<Typography variant="h2">RDM Race App</Typography>
+													<img src={RDMRaceApp} width={100} style={{paddingLeft: "50px", paddingRight: "50px"}} />
+													<Typography variant="h5">Languages:</Typography>
+													<Typography variant="subtitle1">Typescript, JavaScript</Typography>
+													<Typography variant="h5">Frameworks:</Typography>
+													<Typography variant="subtitle1">React, Prisma, MUI</Typography>
+													<Typography variant="body1">RDM Race app is a real time chat app<br/> developed for the Phidipidus race team at the RDM</Typography>
+												</div>
+										</Paper>
+										<Paper elevation={1} style={{width: "100%", height: "100%", display: 'flex',
+											justifyContent: 'center',
+											backgroundColor: '#f2acce',
+											color: "white", textShadow: "black 0.1em 0.1em 0.2em",
+											alignItems: 'center'}}>	
+											<div>
+												<Typography variant="h2">Bi-Os-Coop</Typography>
+												<img src={BiOsCoop} width={100} style={{paddingLeft: "50px", paddingRight: "50px"}}  />
+												<Typography variant="h5">Languages:</Typography>
+												<Typography variant="subtitle1">C#</Typography>
+												<Typography variant="body1">Bi-Os-Coop is a console app<br/> developed for project B</Typography>
+											</div>
+										</Paper>
+										<Paper elevation={0} style={{width: "100%", height: "100%", display: 'flex',
+											justifyContent: 'center',
+											backgroundColor: '#e8f0a1',
+											color: "white", textShadow: "black 0.1em 0.1em 0.2em",
+											alignItems: 'center'}}>	
+											<div style={{margin: '50px'}}>
+												<Typography variant="h2">Makkr</Typography>
+												<img src={Makkr} width={100} style={{paddingLeft: "50px", paddingRight: "50px"}}  />
+												<Typography variant="h5">Languages:</Typography>
+												<Typography variant="subtitle1">C#</Typography>
+												<Typography variant="h5">Frameworks: </Typography>
+												<Typography variant="subtitle1">ASP Web Api, Entity Framework, Xamarin.Forms</Typography>
+												<Typography variant="body1">Makkr is a tinder like meeting app<br/> where elderly people can find like minded individuals to hang out with,<br/> made to solve loneliness</Typography>
+											</div>
+										</Paper>
+									</Carousel>
+								</div>
 						</div>
 					</div>
 				</ParallaxLayer>
@@ -198,7 +248,7 @@ function App() {
 				</ParallaxLayer>
 
 
-				<ParallaxLayer offset={3} speed={0.5} style={{
+				<ParallaxLayer offset={4} speed={0.5} style={{
 						display: 'flex',
 						justifyContent: 'center',
 						alignItems: 'center',
--- src/Typing.tsx
+++ src/Typing.tsx
@@ -3,7 +3,7 @@ import Typewriter from 'typewriter-effect';
 export default function Typing() {
   return (
     <div className="App" style={{display: "inline", fontSize: "x-large"}}>
-      <Typewriter  options={{strings: ["Web", "Mobile", "Desktop"], autoStart: true, loop: true}} />
+      <Typewriter  options={{strings: ["Back-end", "Mobile", "Desktop", "Web"], autoStart: true, loop: true}} />
       <span> Developer</span>
     </div>
   );
